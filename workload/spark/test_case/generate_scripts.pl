ca72e124e6a15b683b25c8f7b5e9b7aba868996a
orcguru <wangting@gmail.com>
2016-11-22 03:56:01 +0800

    Enhance script generation -- run spark works fine

--- workload/spark/test_case/generate_scripts.pl
+++ workload/spark/test_case/generate_scripts.pl
@@ -54,8 +54,31 @@ if ($? != 0) {
 if (not (-e $spark_conf->{"HADOOP_HOME"}."/etc/hadoop/slaves")) {
     die "slaves file not found under ".$spark_conf->{"HADOOP_HOME"}."/etc/hadoop/ folder";
 }
-my $slaves_str = `grep -v # $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves`;
+my $slaves_str = `grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves`;
 my @nodes = split(/\n/, $slaves_str);
+my $all_slaves = "";
+my $first_slave = "";
+my $slave_count = $#nodes + 1;
+foreach my $node (@nodes) {
+    chomp($node);
+    if ($all_slaves eq "") {
+        $all_slaves = $node;
+    } else {
+        $all_slaves = $all_slaves." ".$node;
+    }
+    if ($first_slave eq "") {
+        $first_slave = $node;
+    }
+}
+my $cores_online = `ssh $first_slave ppc64_cpu --cores-on`;
+my $cores = 0;
+if ($cores_online =~ /Number of cores online = ([0-9]+)$/) {
+    $cores = $1;
+}
+my $total_cores_online = $cores * $slave_count;
+if ($cores == 0) {
+    die "Cannot get online cores info from \"ppc64_cpu --cores-on\"";
+}
 push (@nodes, $spark_conf->{"MASTER"});
 if ($#nodes == 0) {
     die "Slave nodes not defined in ".$spark_conf->{"HADOOP_HOME"}."/etc/hadoop/slaves";
@@ -74,6 +97,11 @@ foreach my $node (@nodes) {
             $need_install_tools = 1;
             print "Please install dstat on $node\n";
         }
+        `ssh $node which ppc64_cpu`;
+        if ($? != 0) {
+            $need_install_tools = 1;
+            print "Please install ppc64_cpu on $node\n";
+        }
     }
 }
 if ($ssh_problem != 0) {
@@ -91,10 +119,14 @@ if (not (-e "../../../lpcpu.tar.bz2")) {
 `ssh $nodes[0] "cd /root && tar xjf lpcpu.tar.bz2"`;
 
 ########### Generate the test scripts ############
+# Header
 my $date_str = `date +"%Y%m%d%H%M%S"`;
 chomp($date_str);
 my $script_dir = $case_tag."-".$date_str;
 `mkdir $script_dir`;
+my $current_dir = `pwd`;
+chomp($current_dir);
+my $script_dir_full = $current_dir."/".$script_dir;
 open my $script_fh, "> $script_dir/run.sh" or die "Cannot open file ".$script_dir."/run.sh for write";
 
 my $pmh = dirname(dirname(dirname(`pwd`)));
@@ -107,7 +139,216 @@ export DESCRIPTION="$script_dir"
 export WORKLOAD_DIR="."      # The workload working directory
 export MEAS_DELAY_SEC=1      # Delay between each measurement
 export RUNDIR=\$(\${PMH}/setup-run.sh \$WORKLOAD_NAME)
+
+# SLAVES config required by run-workload.sh
+unset SLAVES
+SLAVES="$all_slaves"
+export SLAVES
+
+cd \$PMH
+
 EOF
+
+# *-scenario.json steps
+my $smt_reset = 0;
+foreach my $step (@{$scenario}) {
+    if (exists $step->{"ACTION"}) {
+        if ($step->{"ACTION"} eq "CLEAR_SWAPPINESS") {
+            print $script_fh <<EOF;
+# ACTION $step->{"ACTION"}
+echo 0 > /proc/sys/vm/swappiness
+grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves | xargs -i ssh {} "echo 0 > /proc/sys/vm/swappiness"
+
+EOF
+        } elsif (($step->{"ACTION"} eq "HDFS") or ($step->{"ACTION"} eq "YARN")) {
+            my $script_name = "dfs";
+            if ($step->{"ACTION"} eq "YARN") {
+                $script_name = "yarn";
+            }
+            my $script_action = "";
+            if (not (exists $step->{"PARAM"})) {
+                close $script_fh;
+                `rm -rf $script_dir_full`;
+                die "ACTION:".$step->{"ACTION"}." require PARAM START or STOP";
+            } elsif ($step->{"PARAM"} eq "START") {
+                $script_action = "start";
+            } elsif ($step->{"PARAM"} eq "STOP") {
+                $script_action = "stop"; 
+            } else {
+                close $script_fh;
+                `rm -rf $script_dir_full`;
+                die "ACTION:".$step->{"ACTION"}." invalid PARAM ".$step->{"PARAM"}.", require PARAM START or STOP";
+            }
+            print $script_fh <<EOF;
+# ACTION $step->{"ACTION"}:$step->{"PARAM"}
+$spark_conf->{"HADOOP_HOME"}/sbin/$script_action-$script_name.sh
+
+EOF
+        } elsif ($step->{"ACTION"} eq "DROP_CACHE") {
+            print $script_fh <<EOF;
+# ACTION $step->{"ACTION"}
+sync && echo 3 > /proc/sys/vm/drop_caches
+grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves | xargs -i ssh {} "sync && echo 3 > /proc/sys/vm/drop_caches"
+
+EOF
+        } elsif ($step->{"ACTION"} eq "WAIT") {
+            my $sec = 5;
+            if (exists $step->{"PARAM"}) {
+                $sec = $step->{"PARAM"};
+            }
+            print $script_fh <<EOF;
+# ACTION $step->{"ACTION"}:$sec
+sleep $sec
+
+EOF
+        } else {
+            close $script_fh;
+            `rm -rf $script_dir_full`;
+            die "ACTION:".$step->{"ACTION"}." is not supported!";
+        }
+    } elsif (exists $step->{"TAG"}) {
+        my $smt_changed = 0;
+        my $tag_idx = 0;
+        if (not (exists $step->{"CMD"})) {
+            close $script_fh;
+            `rm -rf $script_dir_full`;
+            die "Please define CMD section in TAG ".$step->{"TAG"};
+        }
+        my $repeat = 1;
+        if (exists $step->{"REPEAT"}) {
+            $repeat = $step->{"REPEAT"};
+        }
+        my $drop_cache_between_run = 0;
+        if ((exists $step->{"DROP_CACHE_BETWEEN_REPEAT"}) and ($step->{"DROP_CACHE_BETWEEN_REPEAT"} eq "TRUE")) {
+            $drop_cache_between_run = 1;
+        }
+        my $set_smt = 0;
+        if (exists $step->{"SMT"}) {
+            $smt_changed = 1;
+            $smt_reset = 1;
+            $set_smt = $step->{"SMT"};
+        }
+        # Calculate SMT setting if there is "--num-executors" and "--executor-cores" configured in CMD parameter
+        if (exists $step->{"CMD"}->{"PARAM"}) {
+            my $def_num_executors = 0;
+            my $def_executor_cores = 0;
+            foreach my $element (@{$step->{"CMD"}->{"PARAM"}}) {
+                if ($element =~ /--num-executors\s+([0-9]+)/) {
+                    $def_num_executors = $1;
+                }
+                if ($element =~ /--executor-cores\s+([0-9]+)/) {
+                    $def_executor_cores = $1;
+                }
+            }
+            if (($def_num_executors > 0) and ($def_executor_cores > 0)) {
+                $smt_changed = 1;
+                $smt_reset = 1;
+                my $total_cores_required = $def_num_executors * $def_executor_cores;
+                my $smt_ratio = ($total_cores_required * 1.0)/($total_cores_online * 1.0);
+                if ($smt_ratio <= 1.0) {
+                    $set_smt = 1;
+                } elsif ($smt_ratio <= 2.0) {
+                    $set_smt = 2;
+                } elsif ($smt_ratio <= 4.0) {
+                    $set_smt = 4;
+                } elsif ($smt_ratio <= 8.0) {
+                    $set_smt = 8;
+                } else {
+                    close $script_fh;
+                    `rm -rf $script_dir_full`;
+                    die "TAG ".$step->{"TAG"}." --num-executors X --executor-cores exceed available cores in all slaves";
+                }
+            }
+        }
+        print $script_fh <<EOF;
+# TEST STEP:$step->{"TAG"}
+EOF
+        if ($smt_changed == 1) {
+            print $script_fh <<EOF;
+grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves | xargs -i ssh {} "ppc64_cpu --smt=$set_smt"
+EOF
+        }
+        my $cmd = "";
+        if ($step->{"CMD"}->{"COMMAND"} =~ /\<SPARK_HOME\>/) {
+            $step->{"CMD"}->{"COMMAND"} =~ s/\<SPARK_HOME\>/$spark_conf->{"SPARK_HOME"}/;
+        }
+        $cmd = $cmd.$step->{"CMD"}->{"COMMAND"};
+        my $def_conf = 0;
+        if (exists $step->{"CMD"}->{"PARAM"}) {
+            foreach my $element (@{$step->{"CMD"}->{"PARAM"}}) {
+                if (ref($element) eq "HASH") {
+                    if (exists $element->{"--conf"}) {
+                        $def_conf = 1;
+                        foreach my $conf (@{$element->{"--conf"}}) {
+                            $cmd = $cmd." --conf ".$conf;
+                        }
+                        if (exists $spark_conf->{"SPARK_DEFAULTS"}) {
+                            foreach my $key (keys %{$spark_conf->{"SPARK_DEFAULTS"}}) {
+                                $cmd = $cmd." --conf ".$key."=".$spark_conf->{"SPARK_DEFAULTS"}->{$key};
+                            }
+                        }
+                    }
+                } else {
+                    if ($element =~ /\<SPARK_HOME\>/) {
+                        $element =~ s/\<SPARK_HOME\>/$spark_conf->{"SPARK_HOME"}/;
+                    }
+                    $cmd = $cmd." ".$element;
+                }
+            }
+        }
+        if ($def_conf == 0) {
+            print "Spark by default disable event log, however we need that enabled to analyze the result. Did not find \"--conf\" section to add spark event log configuration, are you sure enabled that in spark-defaults.conf ? [Y/N]\n";
+            my $input = <STDIN>;
+            if (($input =~ /N/) or ($input =~ /n/)) {
+                print "Not confirmed, please configure that and run the script again.\n";
+                close $script_fh;
+                `rm -rf $script_dir_full`;
+                exit 1;
+            }
+        }
+        if ($repeat == 1) {
+            print $script_fh <<EOF;
+export RUN_ID=\"$step->{"TAG"}-0\"
+CMD=\"${cmd}\"
+CMD=\"\${CMD} > \$PMH/workload/spark/test_case/$script_dir/$step->{"TAG"}-0.log 2>&1\"
+export WORKLOAD_CMD=\${CMD}
+\${PMH}/run-workload.sh
+
+EOF
+        } else {
+            print $script_fh <<EOF;
+for ITER in \$(seq $repeat)
+do
+EOF
+            if ($drop_cache_between_run == 1) {
+                print $script_fh <<EOF;
+    if [ \$ITER -ne 1 ] 
+    then
+        sync && echo 3 > /proc/sys/vm/drop_caches
+        grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves | xargs -i ssh {} "sync && echo 3 > /proc/sys/vm/drop_caches"
+    fi
+EOF
+            }
+            print $script_fh <<EOF;
+    export RUN_ID=\"$step->{"TAG"}-\$ITER\"
+    CMD=\"${cmd}\"
+    CMD=\"\${CMD} > \$PMH/workload/spark/test_case/$script_dir/$step->{"TAG"}-\$ITER.log 2>&1\"
+    export WORKLOAD_CMD=\${CMD}
+    \${PMH}/run-workload.sh
+done
+
+EOF
+        }
+    }
+}
+
+if ($smt_reset == 1) {
+    print $script_fh <<EOF;
+# ACTION reset to SMT4 on all slave nodes
+grep -v \\# $spark_conf->{"HADOOP_HOME"}/etc/hadoop/slaves | xargs -i ssh {} "ppc64_cpu --smt=4"
+
+EOF
+}
 
 close $script_fh;
 `chmod +x $script_dir/run.sh`;
