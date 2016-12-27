c714510cfeca0523349091414878d86ddf4a2e68
orcguru <wangting@gmail.com>
2016-12-27 10:37:59 +0800

    fix bug

--- workload/spark/scripts/spark_event_log_web_link.pl
+++ workload/spark/scripts/spark_event_log_web_link.pl
@@ -13,6 +13,7 @@ my $debug_fn = $info_fn;
 $debug_fn =~ s/info$/debug.log/;
 
 my $app_count = `grep "TAG:$tag " $info_fn | grep "APPID" | wc -l`;
+chomp($app_count);
 if ($app_count == 0) {
     open DEBUG, ">> $debug_fn" or die "Cannot open file $debug_fn for append";
     print DEBUG "spark_event_log_web_link.pl $info_fn $tag\n";
@@ -23,7 +24,15 @@ if ($app_count == 0) {
 }
 
 my $app_id = `grep "TAG:$tag " $info_fn | grep APPID | head -n 1 | awk '{print \$3}' | awk -F: '{print \$2}'`;
+chomp($app_id);
+my $count = `ps -ef | grep java | grep -i historyserver | grep -v org.apache.hadoop.mapreduce.v2.hs.JobHistoryServer | awk '{print \$2}' | xargs -i sh -c \"netstat -nap | grep {}\" | grep LISTEN | awk '{print \$4}' | awk -F: '{print \$NF}' | wc -l`;
+chomp($count);
+if ($count == 0) {
+    print "NA\n";
+    exit 1;
+}
 my $port = `ps -ef | grep java | grep -i historyserver | grep -v org.apache.hadoop.mapreduce.v2.hs.JobHistoryServer | awk '{print \$2}' | xargs -i sh -c \"netstat -nap | grep {}\" | grep LISTEN | awk '{print \$4}' | awk -F: '{print \$NF}'`;
+chomp($port);
 if ($? != 0) {
     print "NA\n";
     exit 1;
@@ -34,5 +43,6 @@ if ($? != 0) {
     exit 1;
 }
 my $ip = `ip route show | grep ^default | awk '{print \$5}' | xargs -i ifconfig {} | grep netmask | awk '{print \$2}'`;
+chomp($ip);
 print "<a href=\"http://".$ip.":".$port."/history/".$app_id."\">".$app_id."</a>\n";
 exit 0;
--- workload/spark/scripts/summary_count.pl
+++ workload/spark/scripts/summary_count.pl
@@ -13,8 +13,11 @@ my $debug_fn = $info_fn;
 $debug_fn =~ s/info$/debug.log/;
 
 my $success_count = `grep "TAG:$tag " $info_fn | grep "STATUS:0" | wc -l`;
+chomp($success_count);
 my $fail_count = `grep "TAG:$tag " $info_fn | grep "STATUS:1" | wc -l`;
+chomp($fail_count);
 my $total_count = `grep "TAG:$tag " $info_fn | grep "COUNT" | awk '{print \$2}' | awk -F: '{print \$2}'`;
+chomp($total_count);
 if (($success_count + $fail_count) == 0) {
     # Maybe spark standalone scheduler
     my $line = `grep "TAG:$tag " $info_fn | grep EVENTLOG | awk '{print \$3}' | awk -F: '{print \$2}'`;
